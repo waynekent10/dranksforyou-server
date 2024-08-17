5b2e781e289ac2bd71d2c146761a07a5e77899c3
Bruce Clark <bruce.clark55@gmail.com>
2024-08-17 12:19:32 -0400

    added order models and views

---
+++ dranksforyouapi/views/orders.py
@@ -0,0 +1,57 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from dranksforyouapi.models import Order, User
+
+class OrderView(ViewSet):
+    def retrieve(self,request, pk):
+       Order = Order.objects.get(pk=pk)
+       serializer = OrderSerializer(Order, context={'request': request})
+       return Response(serializer.data, status=status.HTTP_200_OK)
+  
+    def list(self, request): 
+        order = Order.objects.all()
+        serializer = OrderSerializer(order, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        """Handle POST operations"""
+        try:
+            # Retrieve the user instance
+            user = User.objects.get(pk=request.data['user_id'])
+            
+            # Create a new order
+            order = Order.objects.create(
+                user_id=user,
+                order_total=request.data['order_total'],
+                payment_type=request.data['payment_type']
+            )
+            
+            # Serialize the order and return the response
+            serializer = OrderSerializer(order)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        except User.DoesNotExist:
+            return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+            
+
+    
+    def destroy(self, request, pk):
+        """Handle DELETE requests to delete an order"""
+        try:
+            order = Order.objects.get(pk=pk)
+            order.delete()
+            return Response({'message': 'Part deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+        except Order.DoesNotExist:
+            return Response({'message': 'Part not found'}, status=status.HTTP_404_NOT_FOUND)
+  
+  
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'user_id', 'order_total', 'payment_type']
+        depth = 2
