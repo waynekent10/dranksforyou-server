7e47e71d1c3ec8523ffd6c67b54935fa3ac4b547
Bruce Clark <bruce.clark55@gmail.com>
2024-08-18 06:42:20 -0400

    added views ob

---
+++ dranksforyouapi/views/order_beverages.py
@@ -0,0 +1,44 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from dranksforyouapi.models import OrderBeverage, Beverage, Order
+
+
+class OrderBeverageView(ViewSet):
+    
+    def retrieve(self, request, pk):
+        try:
+            order_beverage = OrderBeverage.objects.get(pk=pk)
+            serializer = OrderBeverage(order_beverage)
+            return Response(serializer.data)
+        except order_beverage.DoesNotExist:
+            return Response({'message': 'orderbeverage not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+    def list (self, request):
+            order_beverages=OrderBeverage.objects.all()
+            serializer = OrderBeverageSerializer(order_beverages, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+    def create(self, request):
+            order = Order.objects.get(pk=request.data["order_id"])
+            beverage = Beverage.objects.get(pk=request.data['beverage'])
+            
+            order_beverage = OrderBeverage.objects.create(
+                order= order,
+                beverage = beverage
+            )
+            
+            serializer = OrderBeverage(order_beverage)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def destroy(self, request, pk):
+            order_beverage = OrderBeverage.objects.get(pk=pk)
+            order_beverage.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        
+class OrderBeverageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderBeverage
+        fields = ('id', 'order_id', 'beverage_id')
+        depth = 1
+            
